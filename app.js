4c245d8e93013f448f18f6fdc525185146768125
Mary Hall <mh6uj@students-MacBook-Pro.local>
2018-01-20 10:58:56 -0500

    identified letter

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function Hangman (word, guesses, letter) {
 	this.word = word;
 	this.letter = letter;
 	this.guesses = guesses;
-	console.log(this.guesses);
+	// console.log(this.guesses);
 	console.log(this.word);
 
 }
@@ -29,17 +29,39 @@ function Hangman (word, guesses, letter) {
 			transformWord = transformWord.replace(transformWord.charAt(i), "_");
 
 			this.letter = word.charAt(i);
+			//make letter a global var to pass into the guess function
+			letter = this.letter;
+			console.log(this.letter);
 			
 		}
 			console.log(transformWord);
+			// console.log(this.letter);
 			
 		
 	}
 
-	Hangman.guessLetter = function() {
+	// var guess = 9;
 
-	}
+	// Hangman.guessLetter = function() {
+
+	// 	if (guess > 0) {
+
+	// 		inquirer.prompt([
+	// 			{
+	// 				name: "letter",
+	// 				message: "Guess a letter."
+	// 			}
+					
+	// 			]).then(function() {
+	// 				for (i = 0; i < word.length; i++) {
+	// 					if letter === word[i]
+	// 				}
+	// 			})
+	// 		}
+	// 	}
+
+	// }
 
 	Hangman.pickWord();
-	Hangman(word, 9);
+	Hangman(word);
 	// Hangman.wordBlank(word);
