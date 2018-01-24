953b2d51a84463125ecac606b276527eda132d2d
Mary Hall <mh6uj@students-MBP.home>
2018-01-24 15:30:12 -0500

    letters flip

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ var correctLetters = letter.letters;
 var guessFunction = letter.guessFunction;
 var chosenWord = word.chosenWord;
 var displayWord = word.displayWord;
+// var revealWord = letter.letters;
  
 
 //For loop within a for loop for letter array, then blankOutWord. If user input equals, letter in letter array, replace blankOutWord[i] with letter[i]
@@ -13,7 +14,7 @@ var guesses = 9;
 var guessedLetters = [];
 
 function guessLetter () {
-	if (guesses > 0) {
+	if (guesses > 0 && displayWord !== chosenWord) {
 		inquirer.prompt([
 		{
 			name: "guess",
@@ -25,21 +26,28 @@ function guessLetter () {
 				if (inquirerResponse.guess === correctLetters[i]) {
 					console.log(true);
 					// var revealWord = displayWord.replace(displayWord[i], correctLetters[i]);
-					for (i = 0; i < correctLetters.length; i++) {
-						if (correctLetters[i] !== inquirerResponse.guess) {
-						
-						correctLetters[i] = "_";
+					// for (i = 0; i < displayWord.length; i++) {
+						// if (correctLetters[i] !== inquirerResponse.guess) {
+						displayWord = displayWord.replace(displayWord[i], inquirerResponse.guess);
 						// console.log("Something happened");
 
 
-						}	
-					}	
-					
-				}
+						// }	
+					// }	
+				
+				} 
+			} else if (guesses > 0 && inquirerResponse.guess !== correctLetters[i]) {
+				console.log("Sorry, guess again!");
+				guesses--;
+			} else {
+				console.log("Sorry, you lost.");
 			}
+			// console.log(revealWord);
+			console.log(displayWord);
 			console.log(correctLetters);
 			guessedLetters.push(inquirerResponse.guess);
 			console.log("Letters guessed: " + guessedLetters);
+			guessLetter();
 		})
 	}
 }
