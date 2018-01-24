51df6cbd103e199385386d433801d4f0e5c51008
Mary Hall <mh6uj@students-MBP.home>
2018-01-24 12:30:35 -0500

    replaces correct letter

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ var correctLetters = letter.letters;
 // var guessesLeft = letter.guessesLeft;
 var guessFunction = letter.guessFunction;
 var chosenWord = word.chosenWord;
+var displayWord = word.displayWord;
  
 
 //For loop within a for loop for letter array, then blankOutWord. If user input equals, letter in letter array, replace blankOutWord[i] with letter[i]
@@ -23,8 +24,20 @@ function guessLetter () {
 			for (i = 0; i < correctLetters.length; i++) {
 				if (inquirerResponse.guess === correctLetters[i]) {
 					console.log(true);
+					// var revealWord = displayWord.replace(displayWord[i], correctLetters[i]);
+					for (i = 0; i < correctLetters.length; i++) {
+						if (correctLetters[i] !== inquirerResponse.guess) {
+						
+						correctLetters[i] = "_";
+						// console.log("Something happened");
+
+
+						}	
+					}	
+					
 				}
 			}
+			console.log(correctLetters);
 			guessedLetters.push(inquirerResponse.guess);
 			console.log("Letters guessed: " + guessedLetters);
 		})
